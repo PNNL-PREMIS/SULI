e9137ad935936990c75a7a812593bc4e3981adc4
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-06-12 05:49:56 -0400

    Incorporated Jess's inventory changes

--- sla_sampling.R
+++ sla_sampling.R
@@ -5,20 +5,21 @@
 library(readr)
 library(dplyr)
 
-read_csv("SERC storm surge inventory.csv") %>% 
-  filter(!is.na(Plot)) ->
+read_csv("ss-inventory.csv") %>% 
+  filter(!is.na(Plot), Alive == "Yes") ->
   inv
 
 
 # This is REALLY IMPORTANT
-set.seed(951063)
+set.seed(12345)
 
 # Randomly arrange the inventory data
 inv %>%
-  filter(Alive == "Yes") %>% 
+  mutate(Species = substr(Species, 1, 4)) %>% 
+  select(-Date, -JS_codes) %>% 
   group_by(Plot, Species) %>% 
   # sample_frac()'s default is to sample all data = randomly resample rows
   sample_frac() ->
   random_inv
 
-# write_csv(random_inv, "~/Desktop/random.inv.csv")
+# write_csv(random_inv, "~/Desktop/random_inv.csv")
