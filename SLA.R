190fc19b90117063d435dd4e685289ede29d9bab
marideeweber <marideeweber@gmail.com>
2019-06-13 10:46:35 -0400

    Fixed a couple bugs

--- SLA.R
+++ SLA.R
@@ -1,7 +1,8 @@
 
 
-sla <- read.csv("SLA Data/SLA Data.csv")
+sla <- read.csv("SLA Data.csv")
 print(sla)
 
 library(ggplot2)
-qplot(n_Leaves, Leaf.Area..cm2., data = sla)
+p <- qplot(n_Leaves, Leaf.Area..cm2., data = sla)
+print(p)
